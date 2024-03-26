cea6eadbfb0517a0d2b00a3c23f56d05823f0bbd
nzangi <nzangimuoki284@gmail.com>
2024-03-26 23:15:35 +0300

    DONE CRUD on SpringBoot

--- src/main/java/com/nzangi/backend/controllers/EmployeeController.java
+++ src/main/java/com/nzangi/backend/controllers/EmployeeController.java
@@ -4,6 +4,7 @@ import com.nzangi.backend.exception.ResourceNotFoundException;
 import com.nzangi.backend.model.Employee;
 import com.nzangi.backend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,4 +34,29 @@ public class EmployeeController {
                 () -> new ResourceNotFoundException("Employee does not exists with ID:"+id));
         return ResponseEntity.ok(employee);
     }
+    // Update an employee REST API by ID
+    @PostMapping("{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable int id,@RequestBody Employee employeeDetails){
+        Employee updateEmployee = employeeRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Employee not found with id:"+id)
+        );
+
+        updateEmployee.setFirstName(employeeDetails.getFirstName());
+        updateEmployee.setLastName(employeeDetails.getLastName());
+        updateEmployee.setEmailId(employeeDetails.getEmailId());
+        employeeRepository.save(updateEmployee);
+        return ResponseEntity.ok(updateEmployee);
+
+    }
+    // Delete an employee REST API by ID
+    @DeleteMapping("{id}")
+
+    public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable int id){
+        Employee employee = employeeRepository.findById(id).orElseThrow(
+                ()->new ResourceNotFoundException("Employee doesnot exist with id:"+id));
+
+        employeeRepository.delete(employee);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 }
